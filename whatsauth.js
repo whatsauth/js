b94b3f937bed7e98be92941a5dd8843f7e1676cb
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-24 06:55:02 +0700

    del(getCookie):tidak dipakai

--- whatsauth.js
+++ whatsauth.js
@@ -131,21 +131,6 @@ export function deleteCookie(cname) {
     document.cookie = cname + "= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
-function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-    }
-
 function fillformLogin(resjson){
     document.getElementById(id_user).value = resjson.user_name;
     document.getElementById(id_pass).value = resjson.user_pass;
