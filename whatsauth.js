6678cb2000a1e47950e7023cc71754210e220c6c
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-08 12:14:34 +0700

    ga ada lower case

--- whatsauth.js
+++ whatsauth.js
@@ -58,7 +58,7 @@ function generateObjectId(){
     var timestamp = (new Date().getTime() / 1000 | 0).toString(16);
     var randomnum = 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, function() {
         return (Math.random() * 16 | 0).toString(16);
-    }).toLowerCase();
+    });
     return timestamp+randomnum
 }
 
