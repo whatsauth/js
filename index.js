17d5b0fab40e5ae6fec2b7ad8bcb2d31a6141065
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2023-12-16 11:57:22 +0700

    feat: save uuid qr from param

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-import {qrController,deleteCookie} from "./whatsauth.js";
+import {qrController,deleteCookie, getParamsfromURL} from "./whatsauth.js";
 import { wauthparam } from "./config.js";
 
+const main = async () => {
+    const uuidParam = getParamsfromURL().uuid
+    if ( uuidParam !== "") {
+        if (uuidParam.slice(0, 2) !== "v4") {
+            qrController(wauthparam);
+            return;
+        }
+        setCookieWithExpireHour(wauthparam.tokencookiename, uuidParam, wauthparam.tokencookiehourslifetime);
 
-deleteCookie(wauthparam.tokencookiename);
-qrController(wauthparam);
+        return;
+    }
+
+    
+    deleteCookie(wauthparam.tokencookiename);
+    qrController(wauthparam);
+}
+
+
+main();
